c6513e27d80e47a4fce075fd69eb9a768de97656
Lautaro Pereyra <hi@imlautaro.com>
2023-01-24 12:02:03 -0300

    update home page

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
 	<div>
 		<h1>Hello world from Nuxt & Docker!</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+			odio iusto nobis dolores vitae eius ut.
+		</p>
 	</div>
 </template>

21066b2092a45f5a948d7ae5d30a86f94f64a0d8
Lautaro Pereyra <hi@imlautaro.com>
2023-01-24 12:08:46 -0300

    update home page

--- app.vue
+++ app.vue
@@ -5,5 +5,9 @@
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
 			odio iusto nobis dolores vitae eius ut.
 		</p>
+		<p>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
+			ratione rem quis ea harum mollitia excepturi.
+		</p>
 	</div>
 </template>
